7bf216519106f7e5d92a351a028e11cf3377ab16
Widmo206 <widmo206@yahoo.com>
2025-09-10 17:34:05 +0200

    Reworked the player controller as a state machine
    also reworked how some stuff works:
     * the player runs by default now
     * added a crouched state (with standing and walking versions) that use the previous run animation
     * player acceleration is now capped to friction
     * friction has different conditions for switching between dynamic and static (the not-yet-implemented sliding state uses dynamic, everything else uses static)
     * renamed some variables
    
    The feel of the controls hasn't improved...
    
    Hopefully didn't add any new bugs at least

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,4 +1,4 @@
-using Godot;
+﻿using Godot;
 using System;
 using System.Text.RegularExpressions;
 
@@ -8,16 +8,21 @@ using System.Text.RegularExpressions;
 public partial class Player : CharacterBody2D
 {
 	// Physics constants
-	public const float MinWalkingSpeed = 10.0f;			// units per second; the lowest speed at which the Player will render with a walking animation
-	public const float WalkingSpeed = 125.0f;			// units/second; the speed at which the PC will attempt to walk
-	public const float MaxWalkingSpeed = 150.0f;        // units/second; the highest speed at which the PC will be considered as walking
-	public const float RunningSpeed = 250.0f;           // units/second; the speed at which the PC will attempt to run
-	public const float MaxRunningSpeed = 350.0f;        // units/second; the highest speed at which the PC will be considered as running
+	public const float BaseSpeed = 250f;                // units/second; how fast the player walks
+	public const float CrouchSpeed = 125f;              // units/second; how fast the player walks while crouched
+
+
+	//public const float MinWalkingSpeed = 10.0f;         // units per second; the lowest speed at which the Player will render with a walking animation
+	//public const float WalkingSpeed = 125.0f;           // units/second; the speed at which the PC will attempt to walk
+	//public const float MaxWalkingSpeed = 150.0f;        // units/second; the highest speed at which the PC will be considered as walking
+	//public const float RunningSpeed = 250.0f;           // units/second; the speed at which the PC will attempt to run
+	//public const float MaxRunningSpeed = 350.0f;        // units/second; the highest speed at which the PC will be considered as running
 
 	public const float Mass = 40.0f;                    // kilograms;
-	public const float AccelerationForce = 800.0f;      // kg*u / t^2; magnitude of the force applied when the player is accelerating
-	public const float JumpMomentum = 11200.0f;         // kg*u / t; magnitude of the momentum (mass*velocity) applied to the player when jumping
-	public const float AirControlFactor = 1f;         // the PCs acceleration is multiplied by this in the air
+	public const float AccelerationForce = 800.0f;      // kg*u / (s*t); magnitude of the force applied when the player is accelerating
+	                                                    // (yes, I'm mixing time units; velocities are in u/s, but acceleration is applied each tick)
+	public const float JumpMomentum = 11200.0f;         // kg*u / s; magnitude of the momentum (mass*velocity) applied to the player when jumping
+	public const float AirControlFactor = 1f;           // the PCs acceleration is multiplied by this in the air
 
 	public const float DefaultStaticFrictionCoefficient = 0.6f;     // determines friction when walking; higher coefficient => more friction
 	public const float DefaultDynamicFrictionCoefficient = 0.2f;    // determines friction when running; higher coefficient => more friction
@@ -30,21 +35,38 @@ public partial class Player : CharacterBody2D
 	public const int UpdatesPerSecond = 60;
 
 	// other technical shit
-	int movementLock = 0;           // ticks; used for preventing player movement for a set time
-	//string movementState = "standing";
-	//Array movementStates = [""];
+	int movementLock = 0;	// ticks; used for preventing player movement for a set time
+	enum State				// does this count as a constant?
+	{
+		// stationary
+		Idle,
+		Crouching,
+		// moving
+		Walking,
+		Running,
+		Sliding,
+		Jumping,
+		Walljumping,
+		Falling,
+		// interactions
+		Kicking,
+	}
+	State currentState = State.Idle;        // the player's current state
+	int stateLockCountdown = 0;             // ticks; how long until the state can be changed
+
 	bool isRunning = false;
 	bool atRunningSpeed = false;
 	bool atWalkingSpeed = false;
 	bool isKicking = false;
 	bool isGrounded = false;
 	bool canWalljump = false;
-	int airTime = 0;                // ticks; how long has the character spent in the air
-	int jumpBuffer = 0;             // ticks; is a jump action buffered and how much time is left
-	int kickBuffer = 0;             // ticks; is a kick action buffered and how much time is left
-	float facing = 1f;              // 1 = right, -1 = left
-	Vector2 gravityAcceleration = new Vector2(0f, 980f / 60f);      // ~~initialized at _Ready()~~ Initialized now because GetGravity seems to be broken
-																								// (divided by 60 to get u/t^2 instead of u/s^2) // wait shouldn't it be 60^2 ?
+	int airTime = 0;						// ticks; how long has the character spent in the air
+	int jumpBuffer = 0;						// ticks; is a jump action buffered and how much time is left
+	int kickBuffer = 0;						// ticks; is a kick action buffered and how much time is left
+	float facingDirection = 1f;				// 1 = right, -1 = left
+	Vector2 gravityAcceleration = new Vector2(0f, 980f / 60f);		// ~~initialized at _Ready()~~ Initialized now because GetGravity seems to be broken
+																	// (divided by 60 to get u/t^2 instead of u/s^2) // wait shouldn't it be 60^2 ?
+																	// it worked fine so far ¯\_(?)_/¯
 
 	// Node aliases so I don't go insane
 	AnimatedSprite2D PlayerSprite;
@@ -84,27 +106,35 @@ public partial class Player : CharacterBody2D
 		if (isGrounded)
 		{
 			airTime = 0;
+			if (currentState == State.Jumping) {
+				currentState = State.Idle;
+			}
 		}
 		else
 		{
 			velocity += gravityAcceleration;
 			airTime += 1;
-
+			if (velocity.Y > 0) // -y is down
+			{
+				currentState = State.Falling;
+			}
 		}
 
 		// Handle Interaction
 		if (Input.IsActionJustPressed("interact") || kickBuffer > 0) {
-			if (isGrounded && movementLock <= 0)
+			if (isGrounded && stateLockCountdown <= 0)
 			{
-				isKicking = true;
+				currentState = State.Kicking;
 				// kickBuffer = 0;
-				movementLock = InteractionLock;
+				stateLockCountdown = InteractionLock;
 			}
-			else if (kickBuffer <= 0) {
+			else if (Input.IsActionJustPressed("interact")) // checking again so buffer is only updated on button press
+			{
+				// TODO: create a single object for managing input buffers
 				kickBuffer = InputBuffer;
 			}
 		}
-		if (isKicking)
+		if (currentState == State.Kicking)
 		{
 			InteractionCollider.Disabled = false;
 		}
@@ -115,24 +145,25 @@ public partial class Player : CharacterBody2D
 
 
 		// Handle Directional Colliders
-		float direction = 0f;
-		if (movementLock <= 0)
+		float inputDirection = 0f;
+		if (stateLockCountdown <= 0)
 		{
-			direction = Input.GetAxis("move_left", "move_right");
+			inputDirection = Input.GetAxis("move_left", "move_right");
 		}
 		// Not using Mathf.Sign() because I don't want to change with no input
-		if (direction > 0f)
+		if (inputDirection > 0f)
 		{
 			// Facing right
-			facing = 1f;
+			facingDirection = 1f;
 		}
-		else if (direction < 0f)
+		else if (inputDirection < 0f)
 		{
 			// Facing left
-			facing = -1f;
+			facingDirection = -1f;
 		}
+
 		// there's probably a better way, but this was convenient (as in, flipping the parent node)
-		WalljumpDetector.Scale = new Vector2(facing, 1f);
+		WalljumpDetector.Scale = new Vector2(facingDirection, 1f);
 
 
 		// Check for walljumping
@@ -164,93 +195,96 @@ public partial class Player : CharacterBody2D
 		if (Input.IsActionJustPressed("jump") || jumpBuffer > 0)
 		{
 			float JumpVelocity = -JumpMomentum / Mass;
-			if ((isGrounded || airTime < CoyoteTime) && movementLock <= 0)
+			if ((isGrounded || airTime < CoyoteTime) && stateLockCountdown <= 0 && currentState != State.Jumping)
 			{
 				jumpBuffer = 0;
-				airTime += CoyoteTime + 1; // to prevent jumping several times at once
+				// should be unnecessary due to currentState
+				//airTime += CoyoteTime + 1; // to prevent jumping several times at once
 				velocity.Y = JumpVelocity;
+				currentState = State.Jumping;
+				isGrounded = false;
+				// TODO: move SFX to a separate function
 				JumpSFX.PitchScale = 1f + ((float)rand.NextDouble() - 0.5f) * 0.1f;
 				JumpSFX.Play();
 			}
 			else if (canWalljump && movementLock <= 0)
 			{
 				jumpBuffer = 0;
-				movementLock = WalljumpLock;
+				currentState = State.Walljumping;
+				stateLockCountdown = WalljumpLock;
 				// GD.Print("Walljumpng!");
 				// velocity.X *= -0.5f;
 				// Another Desmos equation; 
 				velocity.Y += (1 + Mathf.Atan(velocity.Y * 0.01f) * 2 / Mathf.Pi) * JumpVelocity / Mathf.Sqrt2;
-				velocity.X += facing * JumpVelocity / Mathf.Sqrt2;
-				facing *= -1f; // TIL the decimal point isn't required
+				velocity.X += facingDirection * JumpVelocity / Mathf.Sqrt2;
+				facingDirection *= -1f; // TIL the decimal point isn't required
 			}
-			else if (jumpBuffer <= 0)
+			else if (Input.IsActionJustPressed("jump"))	// checking again so buffer is only updated on button press
 			{
 				jumpBuffer = InputBuffer;
 			}
-		}
-
-		// Handle Running
-		if (Input.IsActionPressed("run"))
-		{
-			isRunning = true;
-		}
-		else {
-			isRunning = false;
-		}
-		if (Mathf.Abs(velocity.X) < MaxWalkingSpeed)
-		{
-			atRunningSpeed = false;
-			atWalkingSpeed = true;
-		}
-		else
-		{
-			atRunningSpeed = true;
-			atWalkingSpeed = false;
 		}
 
 		// Handle Movement
 		
 		// direction moved under "Handle Directional Colliders"
 
-		// maybe move them into the if statement, since I only need one at a time?
-		float dynamicFriction = gravityAcceleration.Y * DefaultDynamicFrictionCoefficient * -Mathf.Sign(velocity.X);
-		float staticFriction = gravityAcceleration.Y * DefaultStaticFrictionCoefficient * -Mathf.Sign(velocity.X); // yeah I know static friction works differently
+		float dynamicFriction = gravityAcceleration.Y * DefaultDynamicFrictionCoefficient;  // magnitude
+		float staticFriction = gravityAcceleration.Y * DefaultStaticFrictionCoefficient;	// yeah I know static friction works differently
 
-		float friction = 0f;
+		float decelerationFriction = 0f;
 		// No friction in the air
 		if (velocity.X != 0f && isGrounded)
 		{
-			if (atWalkingSpeed)
+			if (currentState == State.Sliding)
 			{
-				// Faster slowdown when walking
-				friction = staticFriction;
+				decelerationFriction = dynamicFriction * -Mathf.Sign(velocity.X);
 			}
 			else
 			{
-				friction = dynamicFriction;
+				decelerationFriction = staticFriction * -Mathf.Sign(velocity.X);
 			}
 
-			if (Mathf.Abs(friction) >= Mathf.Abs(velocity.X))
+			if (Mathf.Abs(decelerationFriction) >= Mathf.Abs(velocity.X))
 			{
 				// to not overshoot and accidentally accelerate the other way
-				friction = -velocity.X;
+				decelerationFriction = -velocity.X;
 			}
 		}
 
 		float targetVelocity = 0f;
-		if (direction == 0f)
+		if (inputDirection == 0f)
 		{
-			velocity.X += friction;
+			velocity.X += decelerationFriction;
+			if (isGrounded && stateLockCountdown <= 0) {
+				if (Input.IsActionPressed("crouch"))
+				{
+					currentState = State.Crouching;
+				}
+				else
+				{
+					currentState = State.Idle;
+
+				}
+			}
 		}
 		else
 		{
-			if (isRunning)
+			if (Input.IsActionPressed("crouch"))
 			{
-				targetVelocity = direction * RunningSpeed;
+				targetVelocity = inputDirection * CrouchSpeed;
+				if (isGrounded)
+				{ 
+					currentState = State.Walking;
+				}
 			}
 			else
 			{
-				targetVelocity = direction * WalkingSpeed;
+				targetVelocity = inputDirection * BaseSpeed;
+				if (isGrounded)
+				{
+					currentState = State.Running;
+				}
 			}
 
 			float acceleration = 0f;
@@ -258,11 +292,11 @@ public partial class Player : CharacterBody2D
 			if (Mathf.Abs(velocity.X) >= Mathf.Abs(targetVelocity) && directionAlignedWithVelocity)
 			{
 				// Going faster than desired -> friction to slow down
-				acceleration = friction;
+				acceleration = decelerationFriction;
 			}
 			else
 			{
-				acceleration = direction * AccelerationForce / Mass;
+				acceleration = inputDirection * AccelerationForce / Mass;
 				if (!isGrounded)
 				{
 					acceleration *= AirControlFactor;
@@ -272,56 +306,67 @@ public partial class Player : CharacterBody2D
 				{
 					// Acceleration fall-off when close to desired speed
 					// I used Desmos to find a nice-looking curve
-					// It was supposed to be a convolution to blend with a deceleration curve above targetVelocity, but I couldn't get it to zero at targetVelocity, so I just gave up and made it blend to zero instead
+					// It was supposed to be a convolution to blend with a deceleration curve above targetVelocity, but I couldn't get it to zero at targetVelocity,
+					// so I just gave up and made it blend to zero instead		// actuallynot a problem, since above targetVelocity, friction gets applied
 					// Also, thanks to whoever made that video about convolutions (3B1B ?)
 					acceleration *= 0.5f * (1f - Mathf.Cos(Mathf.Pi * (targetVelocity - velocity.X) / (0.2f * targetVelocity)));
 					// TODO: revisit accel curve so momentum matters more
 				}
-				else if (!directionAlignedWithVelocity && atWalkingSpeed)
-				{
-					// slow down faster when switching direction (only on ground bc air friction == 0)
-					acceleration += friction;
-				}
 			}
-			velocity.X += acceleration;
+			velocity.X += Mathf.Clamp(acceleration, -staticFriction, staticFriction);
 
 			
 		}
 
 
 		// Animation Handling
-		if (isKicking)
+		switch (currentState)
 		{
-			setAnimation("kick");
-		}
-		else if (Mathf.Abs(velocity.X) >= MinWalkingSpeed)
-		{
-			if (atRunningSpeed)
-			{
-				setAnimation("run");
-			}
-			else
-			{
-				setAnimation("walk");
-			}
+			case State.Idle:
+				setAnimation("idle");
+				break;
 
-			// Handling sprite facing; facing == 1f is right
-			if (facing == -1f)
-			{
-				PlayerSprite.FlipH = true;
-			}
-			else if (facing == 1f)
-			{
-				PlayerSprite.FlipH = false;
-			}
+			case State.Crouching:
+				setAnimation("crouched");
+				break;
+
+			case State.Kicking:
+				setAnimation("kicking");
+				break;
+
+			case State.Walking:
+				setAnimation("sneak");
+				break;
+
+			case State.Running:
+				setAnimation("walk");
+				break;
+
+			case State.Sliding:
+				setAnimation("slide");
+				break;
+
+			case State.Jumping:
+				setAnimation("jump");
+				break;
+
+			case State.Walljumping:
+				setAnimation("jump");
+				break;
+
+			case State.Falling:
+				setAnimation("fall");
+				break;
 		}
-		else
+
+		// Handling sprite facing; facing == 1f is right
+		if (facingDirection == -1f)
 		{
-			setAnimation("idle");
+			PlayerSprite.FlipH = true;
 		}
-		if (!IsOnFloor())
+		else if (facingDirection == 1f)
 		{
-			setAnimation("jump");
+			PlayerSprite.FlipH = false;
 		}
 
 		// Variable management
@@ -333,9 +378,9 @@ public partial class Player : CharacterBody2D
 		{
 			kickBuffer--;
 		}
-		if (movementLock > 0)
+		if (stateLockCountdown > 0)
 		{
-			movementLock--;
+			stateLockCountdown--;
 		}
 		else
 		{
@@ -343,8 +388,9 @@ public partial class Player : CharacterBody2D
 		}
 
 		
-		GetNode<Label>("../HUD/PositionDisplay").Text = "Position:\n    x: " + Position.X.ToString() + "\n    y: " + Position.Y.ToString();
-		GetNode<Label>("../HUD/VelocityDisplay").Text = "Velocity:\n    x: " + Velocity.X.ToString() + "\n    y: " + Velocity.Y.ToString();
+		GetNode<Label>("../HUD/PositionDisplay").Text = $"Position:\n    x: {Position.X}\n    y: {Position.Y}";
+		GetNode<Label>("../HUD/VelocityDisplay").Text = $"Velocity:\n    x: {Velocity.X}\n    y: {Velocity.Y}";
+		GetNode<Label>("../HUD/StateDisplay").Text = $"State: {currentState}\nstateLockCountdown: {stateLockCountdown.ToString()}\njumpBuffer: {jumpBuffer}\nkickBuffer: {kickBuffer}";
 
 		Velocity = velocity;
 		MoveAndSlide();
