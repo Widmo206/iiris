a85f1c44e2068d969472d2dd3a27374f66dc184e
Widmo206 <widmo206@yahoo.com>
2025-10-03 18:06:27 +0200

    Made sign text editable in the editor

--- scripts/Sign.cs
+++ scripts/Sign.cs
@@ -5,13 +5,24 @@ public partial class Sign : Node2D
 {
 	Area2D PlayerDetector;
 	Node2D Message;
+	Label Text;
+
+
+	[Export(PropertyHint.MultilineText)]
+	public string TextOverride { get; set; }
 
 
 	public override void _Ready()
 	{
 		PlayerDetector = GetNode<Area2D>("PlayerDetector");
 		Message = GetNode<Node2D>("Message");
+		Text = GetNode<Label>("Message/Text");
 		Message.Visible = false;
+		if (TextOverride != "") 
+		{
+			Text.Text = (string)TextOverride; // workaround; I can't find a way to expose the property directly
+		}
+		
 	}
 
 
